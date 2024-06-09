18d98db7d7aa70f7f9f2c06d92c3bfd6509029bf
robiulalamdev <robiulalam762400@gmail.com>
2024-06-09 15:38:59 +0600

    Claims & Reports done

--- src/pages/DashboardPages/ClaimsReports/ClaimsReports.jsx
+++ src/pages/DashboardPages/ClaimsReports/ClaimsReports.jsx
@@ -1,7 +1,29 @@
 const ClaimsReports = () => {
   return (
-    <div>
-      <h1>ClaimsReports</h1>
+    <div className="flex flex-col justify-between w-full h-full">
+      <h1 className="text-white font-bakbak-one text-[30px] font-normal leading-normal mt-[37px] ml-[549px] text-nowrap">
+        Claims & Reports
+      </h1>
+      <div className="bg-[#121212] rounded-[10px] mt-[46px] w-full h-full flex-grow">
+        <div className="border-r-[1px] border-[#222222] min-w-[403px] max-w-[403px] w-full h-full">
+          <div className="h-[104px] border-b-[1px] border-[#222222] flex items-center pl-[24px]">
+            <h1 className="text-white font-lato text-[30px] font-bold leading-normal">
+              Inbox
+            </h1>
+          </div>
+          <div className="h-[80px] border-b-[1px] border-[#222222] flex items-center pl-[24px]">
+            <h1 className="text-white font-lato text-[12px] font-semibold leading-normal">
+              Removal Request
+            </h1>
+          </div>
+
+          <div className="h-[80px] border-b-[1px] border-[#222222] flex items-center pl-[24px]">
+            <h1 className="text-white font-lato text-[12px] font-semibold leading-normal">
+              Claim Listing
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
