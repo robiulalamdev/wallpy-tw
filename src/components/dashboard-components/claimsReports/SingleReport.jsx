c524974429532717d9dcd5ad5a6b6c49e3d0ec6b
robiulalamdev <robiulalam762400@gmail.com>
2024-06-29 10:42:07 +0600

    claim and reports page ui done

---
+++ src/components/dashboard-components/claimsReports/SingleReport.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { DefaultProfile } from "../../../lib/data/globalData";
+
+const SingleReport = ({ item = null, setSelectedItem, selectedItem }) => {
+  return (
+    <div
+      onClick={() => setSelectedItem(item)}
+      className={`h-[103px] w-full flex items-center pl-[15px] gap-[15px] border-b-[1px] border-[#222222]
+        ${
+          selectedItem?._id === item?._id
+            ? "claimSelectBg"
+            : "hover:bg-[#D9D9D91A] cursor-pointer"
+        }
+        `}
+    >
+      <img
+        src={DefaultProfile}
+        className="w-[40px] h-[40px] rounded-full object-cover"
+      />
+      <h1 className="text-white font-lato text-[14px] font-normal leading-normal twoLine">
+        <p className="font-bold inline">{item?.user?.username}</p>{" "}
+        {item.message}
+      </h1>
+    </div>
+  );
+};
+
+export default SingleReport;
